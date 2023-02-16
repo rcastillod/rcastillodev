ba6d6c6e21e3a7efb59d672aeea73303a9b7bfd4
rcastillod <rodrigo.castillo@global3.cl>
2023-02-16 15:52:24 -0300

    feat: :tada: Begin project

---
+++ seoConfig/index.js
@@ -0,0 +1,12 @@
+export default {
+  title: "rcastillo.Dev",
+  description:
+    "Portfolio Rodrigo Castillo.",
+  og: {
+    title: "rcastillo.Dev - Portfolio Rodrigo Castillo",
+    description:
+      "rcastillo.Dev portfolio made with Nuxt, Vue, and Tailwindcss.",
+    image: "",
+    url: "https://rcastillodev.vercel.app",
+  },
+};
